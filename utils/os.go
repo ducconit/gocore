5bbb982c4fb85df1bde01b91a3689f7b7f08763b
DNT <ducconit@gmail.com>
2024-12-08 16:49:49 +0700

    add utils handler signal

---
+++ utils/os.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func WaitOSSignalHandler(f func(), signals ...os.Signal) {
+	if len(signals) == 0 {
+		return
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), signals...)
+	defer stop()
+	<-ctx.Done()
+	f()
+}
+
+func RegisterOSSignalHandler(f func(), signals ...os.Signal) {
+	if len(signals) == 0 {
+		return
+	}
+
+	go WaitOSSignalHandler(f, signals...)
+}
+
+func RegisterSignalInterruptHandler(f func()) {
+	go WaitOSSignalHandler(f, os.Interrupt, syscall.SIGTERM)
+}
